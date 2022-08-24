ee0fcd3e0fea78cbd21ef3f27bde2b05e9aa84b3
Surajbnp <97525465+Surajbnp@users.noreply.github.com>
2022-08-24 10:32:37 +0530

    routes added

--- src/pages/MainRoutes.jsx
+++ src/pages/MainRoutes.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import {Routes, Route} from 'react-router-dom'
+import HomePage from './HomePage';
+import AudioPage from './AudioPage';
+import Login from './Login';
+import Signup from './Signup';
+import SinglePage from './SinglePage';
+import PaymentPage from './PaymentPage';
 
 const MainRoutes = () => {
-  return <div>MainRoutes</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/audio" element={<AudioPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/audio/:id" element={<SinglePage />} />
+      <Route path="/audio/:id/payment" element={<PaymentPage />} />
+    </Routes>
+  )
 };
 
 export default MainRoutes;
