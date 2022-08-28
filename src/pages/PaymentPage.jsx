1177161511368aa3f992351abb7f7a610807792d
Surajbnp <97525465+Surajbnp@users.noreply.github.com>
2022-08-28 13:37:40 +0530

    g

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,11 +1,11 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
 
 const PaymentPage = () => {
   return (
-    <div>
+    <Box>
       
-      paymentpage
-    </div>
+    </Box>
   )
 }
 
